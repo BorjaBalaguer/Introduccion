e82413297df1e3e4fb654d06ab0e85b127e90d95
BorjaBalaguer <alu6588@ieselcaminas.org>
2022-09-16 13:31:06 +0200

    Cadenas acabado

--- cadenas.php
+++ cadenas.php
@@ -28,4 +28,20 @@ echo substr_count((strtoupper($nombre) . strtolower($nombre)), 'a');
 ?>
 <br>
 <?php
+$pos = stripos($nombre, 'a');
+if ($pos !== false) {
+    echo "La letra 'a' en '$nombre' en la posicio $pos";
+};
+?>
+<br>
+<?php
+echo str_ireplace('o', 0, $nombre);
+?>
+<br>
+<?php
+$url = 'http://username:password@hostname:9090/path?arg=value';
+echo parse_url($url, PHP_URL_SCHEME) . '<br>';
+echo parse_url($url, PHP_URL_USER) . '<br>';
+echo parse_url($url, PHP_URL_PATH) . '<br>';
+echo parse_url($url, PHP_URL_QUERY);
 ?>
