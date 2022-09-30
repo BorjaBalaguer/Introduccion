6a07fdf63c6d5011f3c45dcc2b246a8582520daf
BorjaBalaguer <alu6588@ieselcaminas.org>
2022-09-30 12:30:22 +0200

    csv acabado

---
+++ csv.php
@@ -0,0 +1,9 @@
+<?php
+ $productos = ["1" => "Producto 1", "2" => "Producto 2", "3" => "Producto 3"];
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="productos.csv"');
+
+    foreach ($productos as $key => $value) {
+        echo("$key; $value\n");
+    }
+?>
